ec5a5a029cc36d93101f6d0f2d203ae21ed0453f
James Newton <blackslate@lexogram.com>
2022-12-16 17:30:55 +0300

    props => gridProps

--- imports/ui/components/Calendar.jsx
+++ imports/ui/components/Calendar.jsx
@@ -1,5 +1,44 @@
-/**
+/** 
  * Calendar.jsx
+ *
+ * Displays a calendar for the the period defined by:
+ * - today
+ * - hidePast:      shows days back to Monday this week if false
+ * - daysToDisplay: number of total days to show
+ * 
+ * - Times are shown every 2 columns
+ * - Earliest and latest times depend on availability of
+ *     current teacher
+ *
+ * Future:
+ * + Can scroll to show future and past weeks
+ *
+ * 1. The teacher's name is read in from props
+ * 2. The period of the current display (monday - endTime) and
+ *    the selected timeZone are read in from TimetableContext
+ * 3. These data are passed to the SessionTracker, which returns
+ *    { blocked:     <<< not yet implemented >>>
+ *      day_begin:   (date)Time when teacher starts
+ *      firstHour:   integer number of first hour to display
+ *      hourLine:    row on which to display the first hour
+ *      rows:        total number of rows (used in Grid.jsx >
+ *                   Styles.jsx to calculate the height of the
+ *                   row divs)
+ *      sessions:    array of day column arrays, each day column
+ *                   having at least one a item: the name and date
+ *                   of the day. Column arrays may also contain
+ *                   session objects
+ *      createDated: array of scheduled repeating sessions which
+ *                   occurred in the past, and which need to be
+ *                   converted to a date session.
+ *    }
+ * 4. Calls <Grid /> with the above data to create the calendar
+ * 5. Calls setWeekStart to tell TimetableContext what time the
+ *    current teacher starts the day
+ * 6. Possibly calls the createDatedSession Meteor method (in
+ *    /imports/api/methods/timeTable.js) to convert any
+ *    scheduled repeating sessions to dated sessions, if they
+ *    have already occurred.
  */
 
 import React, { useContext, useEffect } from 'react';
@@ -25,14 +64,17 @@ export const Calendar = (props) => {
     dragState,
   } = props
 
-  let context = useContext(TimetableContext)
+  console.log("props:", props);
+  
+
+  const context = useContext(TimetableContext)
   context.teacher_name = teacher_name
   // {
   //   monday,
   //   endTime,
   //   timeZone,
-  //   setWeekStart
-  //   + props
+  //   setWeekStart,
+  //   + teacher_name from props
   //   + other unused properties
   // }
   const {
@@ -44,7 +86,7 @@ export const Calendar = (props) => {
   } = context
 
 
-  props = useTracker(() => SessionTracker(context))
+  const gridProps = useTracker(() => SessionTracker(context))
   // {
   //   blocked, <<<<
   //   day_begin,
@@ -59,6 +101,9 @@ export const Calendar = (props) => {
   if (!props.sessions) {
     return <h1>{props.error || "Loading..."}</h1>
   }
+
+  console.log("gridProps:", gridProps);
+  
 
 
   const { day_begin, createDated } = props
@@ -85,7 +130,7 @@ export const Calendar = (props) => {
 
   return (
     <Grid
-      {...props}
+      {...gridProps}
     />
   );
 };
--- imports/ui/components/Teacher/Teacher.jsx
+++ imports/ui/components/Teacher/Teacher.jsx
@@ -1,5 +1,19 @@
 /**
  * Teacher.jsx
+ *
+ * Displays:
+ * + Toolbar with:
+ *   - Teacher's name
+ *   - Space for future tools
+ *   - Time Zone selector
+ * + Calendar for the current week and for one week from today
+ * 
+ * Future:
+ * + [New...] button, to create
+ *   - New student
+ *   - New company
+ *   - New school
+ * + Date button to show widget to jump to a specific date
  */
 
 import React from 'react';
@@ -9,10 +23,9 @@ import { Calendar } from '../Calendar'
 import { TeacherToolbar } from './TeacherToolbar'
 
 
-
-
 export const Teacher = () => {
   const { teacher_name } = useParams()
+  
   
   const props = {
     teacher_name,
@@ -21,6 +34,7 @@ export const Teacher = () => {
     dragState:  "all"
   }
 
+  
   return (
     <>
       <TeacherToolbar
